0e8728468623622207f510088b3abcf411ff59d1
Louis Gadza <siuolgadza@gmail.com>
2023-02-10 09:32:05 +0100

    started

---
+++ src/api/users/index.js
@@ -0,0 +1,135 @@
+import express from "express";
+import createHttpError from "http-errors";
+import { adminOnlyMiddleware } from "../../lib/auth/adminOnly.js";
+import { JWTAuthMiddleware } from "../../lib/auth/jwtAuth.js";
+import { createAccessToken } from "../../lib/auth/tools.js";
+import UsersModel from "./model.js";
+import passport from "passport";
+
+const usersRouter = express.Router();
+
+usersRouter.post("/register", async (req, res, next) => {
+  try {
+    const newUser = new UsersModel(req.body);
+    const { _id } = await newUser.save();
+    res.status(201).send({ _id });
+  } catch (error) {
+    next(error);
+  }
+});
+
+usersRouter.get(
+  "/",
+  JWTAuthMiddleware,
+  adminOnlyMiddleware,
+  async (req, res, next) => {
+    try {
+      const users = await UsersModel.find({});
+      res.send(users);
+    } catch (error) {
+      next(error);
+    }
+  }
+);
+usersRouter.get(
+  "/googleLogin",
+  passport.authenticate("google", { scope: ["profile", "email"] })
+);
+usersRouter.get(
+  "/googleRedirect",
+  passport.authenticate("google", { session: false }),
+  async (req, res, next) => {
+    console.log(req.user);
+    // res.send({ accessToken: req.user.accessToken });
+    res.redirect(
+      `${process.env.FE_DEV_URL}?accessToken=${req.user.accessToken}`
+    );
+  }
+);
+
+usersRouter.get("/me", JWTAuthMiddleware, async (req, res, next) => {
+  try {
+    const user = await UsersModel.findById(req.user._id);
+    res.send(user);
+  } catch (error) {
+    next(error);
+  }
+});
+
+usersRouter.put("/me", JWTAuthMiddleware, async (req, res, next) => {
+  try {
+    const updatedUser = await UsersModel.findByIdAndUpdate(
+      req.user._id,
+      req.body,
+      {
+        new: true,
+        runValidators: true,
+      }
+    );
+    res.send(updatedUser);
+  } catch (error) {
+    next(error);
+  }
+});
+
+usersRouter.delete("/me", JWTAuthMiddleware, async (req, res, next) => {
+  try {
+    await UsersModel.findByIdAndUpdate(req.user._id);
+    res.status(204).send();
+  } catch (error) {
+    next(error);
+  }
+});
+
+usersRouter.get("/:userId", JWTAuthMiddleware, async (req, res, next) => {
+  try {
+    const user = await UsersModel.findById(req.params.userId);
+    res.send(user);
+  } catch (error) {
+    next(error);
+  }
+});
+usersRouter.put(
+  "/:userId",
+  JWTAuthMiddleware,
+  adminOnlyMiddleware,
+  async (req, res, next) => {
+    try {
+    } catch (error) {
+      next(error);
+    }
+  }
+);
+
+usersRouter.delete(
+  "/:userId",
+  JWTAuthMiddleware,
+  adminOnlyMiddleware,
+  async (req, res, next) => {
+    try {
+    } catch (error) {
+      next(error);
+    }
+  }
+);
+
+usersRouter.post("/login", async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await UsersModel.checkCredentials(email, password);
+
+    if (user) {
+      const payload = { _id: user._id, role: user.role };
+
+      const accessToken = await createAccessToken(payload);
+      res.send({ accessToken });
+    } else {
+      next(createHttpError(401, "Credentials are not ok!"));
+    }
+  } catch (error) {
+    next(error);
+  }
+});
+
+export default usersRouter;
---
+++ src/api/users/model.js
@@ -0,0 +1,58 @@
+import mongoose from "mongoose";
+import bcrypt from "bcrypt";
+
+const { Schema, model } = mongoose;
+
+const UsersSchema = new Schema(
+  {
+    name: { type: String, required: true },
+    surname: { type: String, required: true },
+    email: { type: String, required: true },
+    password: { type: String, required: false },
+    role: { type: String, enum: ["User", "Admin"], default: "User" },
+    googleId: { type: String, required: false },
+  },
+  { timestamps: true }
+);
+
+UsersSchema.pre("save", async function (next) {
+  const currentUser = this;
+
+  if (currentUser.isModified("password")) {
+    const plainPW = currentUser.password;
+
+    const hash = await bcrypt.hash(plainPW, 11);
+    currentUser.password = hash;
+  }
+
+  next();
+});
+
+UsersSchema.methods.toJSON = function () {
+  const userDocument = this;
+  const user = userDocument.toObject();
+
+  delete user.password;
+  delete user.createdAt;
+  delete user.updatedAt;
+  delete user.__v;
+  return user;
+};
+
+UsersSchema.static("checkCredentials", async function (email, password) {
+  const user = await this.findOne({ email });
+
+  if (user) {
+    const passwordMatch = await bcrypt.compare(password, user.password);
+
+    if (passwordMatch) {
+      return user;
+    } else {
+      return null;
+    }
+  } else {
+    return null;
+  }
+});
+
+export default model("User", UsersSchema);
